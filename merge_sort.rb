0ad8a41d4e352519e63519a141e2f564968e8b3a
kareem <flytokareem@gmail.com>
2019-08-26 12:03:00 +0530

    merge sort added

---
+++ merge_sort.rb
@@ -0,0 +1,20 @@
+def mergesort(*arr)
+    return arr if arr.size <= 1
+    mid   = arr.size / 2
+    left  = arr[0..mid]
+    right = arr[mid..arr.size]
+    merge(mergesort(left), mergesort(right))
+  end
+  
+  def merge(left, right)
+    sorted = []
+    until left.empty? || right.empty?
+      if left.first <= right.first
+        sorted << left.shift
+      else
+        sorted << right.shift
+      end
+    end
+    sorted.concat(left).concat(right)
+  end
+  p mergesort [81,21,73,4,55,26,47,78]
